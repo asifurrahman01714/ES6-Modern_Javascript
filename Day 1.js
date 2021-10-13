436fd816ece6f219b993f4af9e3b7d4eba953f1b
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-13 21:07:21 +0600

    Converting a normal function to arrow function with one return value only

---
+++ Day 1.js
@@ -0,0 +1,10 @@
+// Convert normal function to arrow function
+function number() {
+    return 10;
+}
+console.log(number());
+
+// Converting it to arrow function
+
+const number1 = ()=> 10;
+console.log(number1());
